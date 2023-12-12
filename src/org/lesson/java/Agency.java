36b25e3f61c81f787113aef88bfccb1244d9dab3
buonoalex <loopatthecity@gmail.com>
2023-12-12 13:02:28 +0100

    Finish Milestone 1

--- src/org/lesson/java/Agency.java
+++ src/org/lesson/java/Agency.java
@@ -1,11 +1,10 @@
 package org.lesson.java;
 
 import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
 import java.util.Scanner;
 
 public class Agency {
-    public static void main(String[] args) {
+    public static void main(String[] args){
         //Apro Scanner
         Scanner myBroScan = new Scanner(System.in);
 
@@ -13,21 +12,25 @@ public class Agency {
         while (!sentinella){
             System.out.println("Vuoi inserire una nuova vacanza (y/n) : ");
             String Answer = null;
-            try {
-                Answer = myBroScan.nextLine();
-            }catch (Exception e1){
-                System.err.println("Invalid type");
+            boolean flagAnswer = false;
+            while (!flagAnswer) {
+                try {
+                    Answer = myBroScan.nextLine();
+                    flagAnswer = true;
+                } catch (Exception e) {
+                    System.err.println("Invalid type");
+                }
             }
             if (Answer.equals("y")){
 
                 //Input Destinazione
                 System.out.println("Inserisci la tua destinazione : ");
-                boolean flegDestinationAnswer = false;
+                boolean flagDestinationAnswer = false;
                 String destinationAnswer = null;
-                while (!flegDestinationAnswer){
+                while (!flagDestinationAnswer){
                     try {
                         destinationAnswer = myBroScan.nextLine();
-                        flegDestinationAnswer = true;
+                        flagDestinationAnswer = true;
                     } catch (Exception e) {
                         System.err.println("Invalid type");
                     }
@@ -37,27 +40,24 @@ public class Agency {
                 System.out.println("Inserisci la data di partenza della tua vacanza (yyyy-MM-dd) : ");
                 boolean flegstartDateAnswer = false;
                 LocalDate startDateAnswer = null;
-                String inputDate = null;
                 while (!flegstartDateAnswer) {
                     try {
-                        inputDate = myBroScan.nextLine();
-                        DateTimeFormatter form = DateTimeFormatter.ofPattern(inputDate);
-                        startDateAnswer = LocalDate.parse(inputDate,form);
+                        startDateAnswer = LocalDate.parse(myBroScan.nextLine());
                         flegstartDateAnswer = true;
-                    } catch (Exception e2) {
+                    } catch (Exception e) {
                         System.out.println("La data non è corretta");
                     }
                 }
 
                 //Input Data ritorno
-                System.out.println("Inserisci la data di ritorno della tua vacanza : ");
+                System.out.println("Inserisci la data di ritorno della tua vacanza : (yyyy-MM-dd) ");
                 boolean flegfinishDateAnswer = false;
                 LocalDate finishDateAnswer = null;
                 while (!flegfinishDateAnswer) {
                     try {
                         finishDateAnswer = LocalDate.parse(myBroScan.nextLine());
                         flegfinishDateAnswer = true;
-                    } catch (Exception e3) {
+                    } catch (Exception e) {
                         System.out.println("La data non è corretta");
                     }
                 }
@@ -65,9 +65,11 @@ public class Agency {
                 //Creazione Vacanza
                 try{
                     Vacation vacationUser = new Vacation(destinationAnswer,startDateAnswer,finishDateAnswer);
-                }catch (Exception e4){
-                    System.out.println("La creazione non ha avuto successo");
+                    vacationUser.PrintVacation();
+                }catch (Exception e){
+                    System.err.println("La creazione non ha avuto successo");
                 }
+
             } else if (Answer.equals("n")) {
                 System.out.println("Arrivederci");
                 sentinella = true;
